d26fe463ff5bfa4ebf73b4c482d7c9d0cfa326e1
hardkore05 <trashurban@gmail.com>
2025-11-07 11:54:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Ruta absoluta hacia la carpeta "public"
+const publicPath = path.join(__dirname, "src", "public");
+
 // Servir la carpeta "public" como contenido estático
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(publicPath));
 
 // Ruta principal: sirve el index.html
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
 // Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
